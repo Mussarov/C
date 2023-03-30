89cc38bbee991114930e856eb66fe2240f1cbc80
Mussarov <128660971+Mussarov@users.noreply.github.com>
2023-03-30 12:32:10 +0900

    Add files via upload

---
+++ C_5/test2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (void)
+{
+ int number;
+   printf("정수를 입력하시오:");
+   scanf ("%d",&number);
+
+   if (number>0)
+       printf ("양수이다");
+
+
+    printf ("입력된 값은 %d입니다.", number);
+
+
+       return 0;
+
+}
